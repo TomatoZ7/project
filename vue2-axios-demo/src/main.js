c766f4ed4bb215dc49b306ccab0c3fec021aed65
TomatoZ7 <17612064467@163.com>
2021-08-15 22:08:21 +0800

    vue2-axios-demo init

--- vue2-axios-demo/src/main.js
+++ vue2-axios-demo/src/main.js
@@ -1,10 +1,41 @@
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+// axios({
+//   url: 'http://123.207.32.32:8000/home/multidata'
+// }).then(res => {
+//   console.log(res)
+// })
+
+axios.defaults.baseURL = 'http://123.207.32.32:8000'
+axios.defaults.timeout = 5000   // ms
+
+axios.all(
+    [ 
+      axios({
+        url: '/home/multidata'
+      }),
+      axios({
+        url: '/home/data',
+        params: {
+          type : 'sell',
+          page: 5
+        }
+      })
+    ]
+  )
+  // .then(res => {
+  //   console.log(res)
+  // })
+  .then(axios.spread((res1, res2) => {
+    console.log(res1)
+    console.log(res2)
+  }))
