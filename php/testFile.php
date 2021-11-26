256c579d332fcd6af58730aefd1b5a08ecbe58cb
zhouhongzhi <zhouhongzhi@myzaker.com>
2021-11-26 13:39:26 +0800

    file system

--- php/testFile.php
+++ php/testFile.php
@@ -4,7 +4,11 @@ try {
 
     $file = fopen('./test.txt', 'r');
 //echo fwrite($file, '你好，世界', 3);
-     var_dump(fgetc($file));
+//    while (!feof($file)) {
+//        $c = fgetc($file);
+//        echo($c);
+//    }
+    echo fread($file, 3);
     fclose($file);
 
 } catch (\Throwable $throwable) {
