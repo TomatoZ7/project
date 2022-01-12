371651f8e83ed0cb52f44f59c5df194df4121daa
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-01-12 09:46:28 +0800

    pillow

--- py/test/pillow_demo.py
+++ py/test/pillow_demo.py
@@ -2,7 +2,7 @@
 import warnings
 import os
 import matplotlib.pyplot as plt  # 图像处理/展现的相关函数库
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 warnings.filterwarnings('ignore')
 
@@ -60,7 +60,39 @@ image = Image.open(test_image)
 # 左右互换
 # image_flip = image.transpose(Image.FLIP_LEFT_RIGHT)
 # 上下互换
-image_flip = image.transpose(Image.FLIP_TOP_BOTTOM)
+# image_flip = image.transpose(Image.FLIP_TOP_BOTTOM)
 
-plt.imshow(image_flip)
+# 在图像上绘图
+# # 产生一个有 4 个颜色 channels 的空白图像
+# blank_image = Image.new('RGBA', (400, 300), 'white')
+# # 在 blank_image 图像上绘图
+# img_draw = ImageDraw.Draw(blank_image)
+# # 画一个矩形
+# img_draw.rectangle((70, 50, 270, 200), outline='red', fill='blue')
+# # 取得字形物件
+# fnt = ImageFont.truetype('/System/Library/Fonts/Monaco.ttf', 40)
+# # 放上文字信息到图像
+# img_draw.text((70, 250), 'hello world', font=fnt, fill='green')
+
+# # 产生一个有 4 个颜色 channels 的空白图像
+# blank_image = Image.new('RGBA', (400, 300), 'white')
+# # 在 blank_image 图像上绘图
+# img_draw = ImageDraw.Draw(blank_image)
+# # 在 PIL 中可以用 rectangle 来画一个四方形，但是无法控制框线的粗细
+# img_draw.rectangle((70, 50, 270, 200), outline=None, fill='pink')
+# # 通过画线来画一个四方框的框线并控制粗细
+# img_draw.line([(70, 50), (270, 50), (270, 200), (70, 200), (70, 50)], fill='red', width=4)
+# # 在 PIL 中要画一个可以控制大小的图要通过以下方式
+# r = 10  # 设定半径
+# # 以图的中心点(x, y)来计算框住图的边界框坐标[(x1, y1), (x2, y2)]
+# img_draw.ellipse((270-r, 200-r, 270+r, 200+r), fill='orange')
+# # 画一个多边形
+# img_draw.polygon([(40, 40), (40, 80), (80, 60), (60, 40)], fill='green', outline=None)
+
+# 颜色变换
+# 将彩色的转换成灰阶
+greyscale_image = image.convert('L')
+# 注意要注明 cmap='gray' 才能正确 show 出灰阶图像
+
+plt.imshow(greyscale_image, cmap='gray')
 plt.show()
