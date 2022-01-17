47d36d4fef07ceba08269cdb22c19bf858f597fe
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-01-17 10:07:57 +0800

    test.py

--- py/test/test.py
+++ py/test/test.py
@@ -1,9 +1,28 @@
 import requests
 from selenium import webdriver
 
-options = webdriver.ChromeOptions()
-options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-driver = webdriver.Chrome(executable_path=r'/Users/tz7/Tools/chromedriver_97', chrome_options=options)
+# options = webdriver.ChromeOptions()
+# options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+# driver = webdriver.Chrome(executable_path=r'/Users/tz7/Tools/chromedriver_97', chrome_options=options)
+#
+# driver.maximize_window()
+# driver.get('https://www.baidu.com/')
+import re
 
-driver.maximize_window()
-driver.get('https://www.baidu.com/')
+string = "abcdfphp345pythony_py"
+
+pattern1 = "p.*y"  # 贪婪模式
+res1 = re.search(pattern1, string)
+print(res1)  # <re.Match object; span=(5, 21), match='php345Pythony_py'>
+
+pattern2 = "p.*?y"
+res2 = re.search(pattern2, string, re.I)
+print(res2)  # <re.Match object; span=(5, 13), match='php345Py'>
+
+# pattern3 = "cd{3}"
+# res3 = re.search(pattern3, string)
+# print(res3)  # <re.Match object; span=(2, 6), match='cddd'>
+#
+# pattern4 = "cd{2,}"
+# res4 = re.search(pattern4, string)
+# print(res4)  # <re.Match object; span=(2, 6), match='cddd'>
