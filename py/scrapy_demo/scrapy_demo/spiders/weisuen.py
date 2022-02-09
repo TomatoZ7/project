bfa414c7622aebee9e127bc589664a374e4a2cdf
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-02-09 22:06:01 +0800

    qidian novel

--- py/scrapy_demo/scrapy_demo/spiders/weisuen.py
+++ py/scrapy_demo/scrapy_demo/spiders/weisuen.py
@@ -9,6 +9,15 @@ class WeisuenSpider(scrapy.Spider):
         'http://slide.mil.news.sina.com.cn/k/slide_8_193_45192.html#p=1',
         'http://news.sina.com.cn/pl/2016-09-12/doc-ifxvukhv8147404.shtml'
     ]
+    urls2 = (
+        'http://www.jd.com',
+        'http://sina.com.cn',
+    )
+
+    def start_requests(self):
+        for url in self.urls2:
+            # 调用默认的 make_requests_from_url() 方法生成具体请求并通过 yield 返回
+            yield self.make_requests_from_url(url)
 
     def parse(self, response):
         url_name = response.xpath('/html/head/title/text()')
