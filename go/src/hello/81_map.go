9747640d8cbf4f8e5dc8064f818d23fe7decfa91
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-05-02 10:15:53 +0800

    go

---
+++ go/src/hello/81_map.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	//makeMaps()
+	//mapTestElement()
+	//mapRange()
+	//mapRange2()
+	sortMap()
+}
+
+func makeMaps() {
+	var mapLit map[string]int
+	var mapAssigned map[string]int
+
+	mapLit = map[string]int{"one": 1, "two": 2}
+	mapCreated := make(map[string]float32)
+	mapAssigned = mapLit
+
+	mapCreated["key1"] = 4.5
+	mapCreated["key2"] = 3.14159
+	mapAssigned["two"] = 3
+
+	fmt.Printf("Map literal at \"one\" is: %d\n", mapLit["one"])
+	fmt.Printf("Map created at \"key2\" is: %f\n", mapCreated["key2"])
+	fmt.Printf("Map assigned at \"two\" is: %d\n", mapAssigned["two"])
+	fmt.Printf("Map literal at \"ten\" is: %d\n", mapLit["ten"])
+}
+
+func mapTestElement() {
+	var value int
+	var isPresent bool
+
+	map1 := make(map[string]int)
+	map1["New Delhi"] = 55
+	map1["Beijing"] = 20
+	map1["Washington"] = 25
+	value, isPresent = map1["Beijing"]
+	if isPresent {
+		fmt.Printf("The value of \"Beijing\" in map1 is: %d\n", value)
+	} else {
+		fmt.Printf("map1 does not contain Beijing")
+	}
+
+	value, isPresent = map1["Paris"]
+	fmt.Printf("Is \"Paris\" in map1 ?: %t\n", isPresent)
+	fmt.Printf("Value is: %d\n", value)
+
+	// delete an item
+	delete(map1, "Washington")
+	value, isPresent = map1["Washington"]
+	if isPresent {
+		fmt.Printf("The value of \"Washington\" in map1 is: %d\n", value)
+	} else {
+		fmt.Println("map1 does not contain Washington")
+	}
+}
+
+func mapRange() {
+	capitals := map[string]string{"France": "Paris", "Italy": "Rome", "Japan": "Tokyo"}
+	for key := range capitals {
+		fmt.Println("Map item: Capital", key, "is", capitals[key])
+	}
+}
+
+func mapRange2() {
+	// Version A
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][3] = 2
+	}
+	fmt.Printf("Version A: Value of items: %v\n", items)
+
+	// Version B
+	items2 := make([]map[int]int, 5)
+	for _, item := range items2 {
+		item = make(map[int]int, 1)	// item is only a copy of the slice element
+		item[1] = 2
+	}
+	fmt.Printf("Version B: Value of items: %v\n", items2)
+}
+
+func sortMap() {
+	var (
+		barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+			"delta": 87, "echo": 56, "foxtrot": 12,
+			"golf": 34, "hotel": 16, "indio": 87,
+			"juliet": 65, "kili": 43, "lima": 98}
+	)
+
+	fmt.Println("unsorted:")
+	for k, v := range barVal {
+		fmt.Printf("Key: %v, Value: %v \n", k, v)
+	}
+
+	keys := make([]string, len(barVal))
+	i := 0
+	for k, _ := range barVal {
+		keys[i]	= k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println()
+	fmt.Println("sorted:")
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v /", k, barVal[k])
+	}
+}
+
+func invertMap() {
+	var (
+		barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+			"delta": 87, "echo": 56, "foxtrot": 12,
+			"golf": 34, "hotel": 16, "indio": 87,
+			"juliet": 65, "kili": 43, "lima": 98}
+	)
+
+	invMap := make(map[int]string, len(barVal))
+	for k, v := range barVal {
+		invMap[v] = k
+	}
+	fmt.Println("inverted:")
+	for k, v := range invMap {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+}
