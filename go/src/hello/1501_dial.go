50ad94a1dae3c4f20413fe83d2b360f395ff80b9
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-05-29 23:09:22 +0800

    go

---
+++ go/src/hello/1501_dial.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "192.0.32.10:80")	// tcp ipv4
+	checkConnection(conn, err)
+	conn1, err1 := net.Dial("udp", "192.0.32.10:80")	// tcp ipv4
+	checkConnection(conn1, err1)
+	conn2, err2 := net.Dial("tcp", "[2620:0:2d0:200::10]:80")	// tcp ipv6
+	checkConnection(conn2, err2)
+}
+
+func checkConnection(conn net.Conn, err error) {
+	if err != nil {
+		fmt.Printf("error %v connecting!", err)
+		os.Exit(1)
+	}
+	fmt.Println("ok")
+}
