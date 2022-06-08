0b945ed786ed0d55ad6ca6fe3c588c105b8b88a5
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-06-08 22:22:05 +0800

    go

--- go/src/hello/temp.go
+++ go/src/hello/temp.go
@@ -5,22 +5,42 @@ import (
 	"math"
 )
 
-type Vertex struct{
-	X, Y float64
+type Abser interface {
+	Abs() float64
 }
 
 func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
 	v := Vertex{3, 4}
-	v.Scale(10)
+
 	fmt.Println(v.Abs())
+
+	a = f  // a MyFloat 实现了 Abser
+	a = &v // a *Vertex 实现了 Abser
+
+	fmt.Println(a)
+
+	// 下面一行，v 是一个 Vertex（而不是 *Vertex）
+	// 所以没有实现 Abser。
+	//a = v
+	//
+	//fmt.Println(a.Abs())
 }
 
-func (v Vertex) Abs() float64 {
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
-
-func (v Vertex) Scale(f float64) {
-	v.X *= f
-	v.Y *= f
-}
-
