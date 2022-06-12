f5fdfb3d27fa113f6415d70103d07025e3c86f60
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-06-12 22:47:01 +0800

    go

--- go/src/hello/temp.go
+++ go/src/hello/temp.go
@@ -4,13 +4,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	input := os.Args
+	joint(input)
+	stringJoin(input)
+}
+
+func joint(param []string) {
+	start := time.Now()
 	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
+	for i := 1; i < len(param); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+	cost := time.Since(start)
+	fmt.Println(cost)
+}
+
+func stringJoin(param []string) {
+	start := time.Now()
+	res := strings.Join(param, "")
+	fmt.Println(res)
+	cost := time.Since(start)
+	fmt.Println(cost)
 }
