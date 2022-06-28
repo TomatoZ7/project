f18ea592257ce5d17602d737fda2e7bf5b90dcc8
zhouhongzhi <zhouhongzhi@myzaker.com>
2022-06-28 23:56:51 +0800

    go

---
+++ go-bible/src/ch4/dedup.go
@@ -0,0 +1,24 @@
+package ch4
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Dedup() {
+	seen := make(map[string]bool)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if !seen[line] {
+			seen[line] = true
+			fmt.Println(line)
+		}
+	}
+
+	if err := input.Err(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
+		os.Exit(1)
+	}
+}
--- go-bible/src/main.go
+++ go-bible/src/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"./ch4"
-	"fmt"
 )
 
 func main()  {
-	data := []int{1, 2, 3, 4, 5}
-	fmt.Println(ch4.RotateByCycle(data, 2))
+	ch4.CharCount()
 }
