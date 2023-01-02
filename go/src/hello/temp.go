cda09741beba4ad2b3ca12ea73514a6dfed484fb
zhouhongzhi <zhouhongzhi@myzaker.com>
2023-01-02 22:20:22 +0800

    Go

--- go/src/hello/temp.go
+++ go/src/hello/temp.go
@@ -1,14 +1,43 @@
-// Echo1 prints its command-line arguments.
 package main
 
 import "fmt"
 
-func main() {
-	printArr([]int{1, 2})
+type Pet interface {
+	Name() string
+	Category() string
 }
 
-func printArr[T int](arr []T) {
-	for _, a := range arr {
-		fmt.Println(a)
+type Dog struct {
+	name string // 名字。
+}
+
+func (dog *Dog) SetName(name string) {
+	dog.name = name
+}
+
+func (dog Dog) Name() string {
+	return dog.name
+}
+
+func (dog Dog) Category() string {
+	return "dog"
+}
+
+func main() {
+
+	value2 := [...]int8{0, 1, 2, 3, 4, 5, 6}
+	switch value2[4] {
+	case 0, 1:
+		fmt.Println("0 or 1")
+	case 2, 3:
+		fmt.Println("2 or 3")
+	case 4, 5, 6:
+		fmt.Println("4 or 5 or 6")
 	}
+}
+
+func modify(param [3][]string) [3][]string {
+	//param[0] = []string{"x", "y", "z"}
+	param[0][1] = "xxx"
+	return param
 }
